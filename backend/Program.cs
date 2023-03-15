e31a50c1295c2d9a6852bdfaf83935bd7b5f6ed3
kry127 <kry127@yandex.ru>
2023-03-16 00:09:21 +0300

    Finally add swagger

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,11 +7,30 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo { Title = "gRPC transcoding", Version = "v1" });
+
+    var filePath = Path.Combine(System.AppContext.BaseDirectory, "backend.xml");
+    c.IncludeXmlComments(filePath);
+    c.IncludeGrpcXmlComments(filePath, includeControllerXmlComments: true);
+});
 
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGet("/", () => """
+To use Swagger navigate here: http://localhost:5231/swagger/
+Assuming that your server runs on localhost:5231.
+""");
+
 
 app.Run();
